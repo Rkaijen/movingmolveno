ca5a7ca300f08835208f28787c024f41ce6b750a
Andries <andries_aarts1986@hotmail.com>
2019-03-08 16:40:45 +0100

    Andries: vehicle GET ALL en GET ONE

---
+++ rest-api/vehicle/getall.js
@@ -0,0 +1,49 @@
+"use strict";
+
+const mysql = require("mysql");
+
+let express = require("express");
+
+let app = express();
+
+let bodyParser = require("body-parser");
+
+const fn = require('../../private/functions');
+
+app.use(bodyParser.json());
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+const connection = fn.dbconnection(mysql);
+
+connection.connect((err) => {
+
+  if (err) {
+    throw err;
+  }
+
+  else {
+    console.log('Connected!');
+  }
+});
+
+app.get('/api/vehicle', function(req, res) {
+
+  res.setHeader('Content-Type', 'application/json');
+
+  connection.query('SELECT * FROM vehicle', (err, vehicle) => {
+
+    if (!err) {
+      res.end(JSON.stringify(vehicle));
+    }
+
+    else {
+      throw err;
+    }
+  });
+});
