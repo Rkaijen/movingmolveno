b8acf919263b09f7edcd060c10abab86e46e3155
rkaijen <rob.kayen@gmail.com>
2019-03-08 14:37:13 +0100

    added first versions of rest-api files/folders

---
+++ private/functions.js
@@ -0,0 +1,12 @@
+const dbserver = require('./db');
+
+exports.dbconnection = function(mysql) {
+  // MYSQL CONNECT
+  const connection = mysql.createConnection({
+    host: dbserver.db().host,
+    user: dbserver.db().user,
+    password: dbserver.db().pwrd,
+    database: dbserver.db().name
+  });
+  return connection;
+}
